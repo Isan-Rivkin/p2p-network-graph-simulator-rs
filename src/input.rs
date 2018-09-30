f8432447d08464ad52072d519f00009b9a9f67c0
Isan-Rivkin <isanrivkin@gmail.com>
2018-09-30 11:04:06 +0300

    refactor the code

---
+++ src/input.rs
@@ -0,0 +1,37 @@
+use std::error::Error;
+use csv;
+
+pub type OptimalOut = usize; 
+pub type MaxIn = usize;
+
+pub fn csv_to_graph(path : &str)->Result<(Vec<usize>,Vec<Vec<usize>>,OptimalOut,MaxIn),Box<Error>> {
+    
+    // Build the CSV reader and iterate over each record.
+    let mut rdr = csv::ReaderBuilder::new().
+        flexible(true).
+        from_path(path)?;
+
+    let mut graph : Vec<Vec<usize>> = Vec::new();
+
+    for (idx,result) in rdr.records().enumerate(){
+
+        let line = result?;
+        graph.push(Vec::new());
+
+        for element in &line {
+            let num =  element.to_string().parse::<usize>()?;
+            graph[idx].push(num);
+        }
+    }
+    // get the dns nodes 
+    if let Some(mut dns_nodes) = graph.pop(){
+        if let Some(max_in) = dns_nodes.pop(){
+
+            if let Some(optimal) = dns_nodes.pop(){
+                return Ok((dns_nodes,graph, optimal, max_in))
+            }
+        }
+    }
+
+    Err(From::from("expected at least one record but got none"))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,11 @@
-mod policy;
-use policy::Policy;
-
 extern crate csv;
 
+mod input;
+mod policy;
+
+use policy::Policy;
+use input::*;
 use std::error::Error;
-
-type optimal_out = usize; 
-type max_in = usize;
-
-pub fn csv_to_graph(path : &str)->Result<(Vec<usize>,Vec<Vec<usize>>,optimal_out,max_in),Box<Error>> {
-    
-    // Build the CSV reader and iterate over each record.
-    let mut rdr = csv::ReaderBuilder::new().
-        flexible(true).
-        from_path(path)?;
-
-    let mut graph : Vec<Vec<usize>> = Vec::new();
-
-    for (idx,result) in rdr.records().enumerate(){
-
-        let line = result?;
-        graph.push(Vec::new());
-
-        for element in &line {
-            let num =  element.to_string().parse::<usize>()?;
-            graph[idx].push(num);
-        }
-    }
-    // get the dns nodes 
-    if let Some(mut dns_nodes) = graph.pop(){
-        if let Some(max_in) = dns_nodes.pop(){
-
-            if let Some(optimal) = dns_nodes.pop(){
-                return Ok((dns_nodes,graph, optimal, max_in))
-            }
-        }
-    }
-
-    Err(From::from("expected at least one record but got none"))
-}
-
 
 fn main() {
 
